302753d80aedcd39d8486f635fef7df222b700bc
Bestian Tang <bestian@gmail.com>
2021-02-03 10:00:36 +0800

    moving hulk

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import Vue2TouchEvents from 'vue2-touch-events'
+Vue.use(Vue2TouchEvents, {
+    disableClick: false,
+    touchClass: '',
+    tapTolerance: 10,
+    touchHoldTolerance: 400,
+    swipeTolerance: 30,
+    longTapTimeInterval: 400
+})
 
 Vue.config.productionTip = false
 
