cde29a23a8002afe9c3fd0b4d6801e93be8d7af1
Bestian Tang <bestian@gmail.com>
2021-02-03 10:11:14 +0800

    add devil

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello" @keydown="move($event)" tabindex="0" v-touch:swipe.left="dx" v-touch:swipe.right="dx2" v-touch:swipe.up="dy" v-touch:swipe.down="dy2">
-    <img id="hulk" src="/img/hulk.jpg" :style="{top: t + 'px', left:t2 + 'vw'}">
+    <img id="hulk" src="/img/hulk2.jpg" :style="{top: t + 'px', left:t2 + 'vw'}">
+    <img class="devil" v-for = "(d, idx) in devils" :key="idx" src="/img/devil.jpg" :style="{top: d.t + 'px', left:d.t2 + 'vw'}">
   </div>
 </template>
 
@@ -13,10 +14,25 @@ export default {
   data () {
     return {
       t: 0,
-      t2: 0
+      t2: 0,
+      devils: []
     }
   },
+  mounted () {
+    this.devils = [0, 1, 2, 3, 4, 5].map(function () {
+        return {
+          t: Math.floor(Math.random()*500),
+          t2: Math.floor(Math.random()*100)
+        }
+    })
+  },
   methods: {
+    kill () {
+      var vm = this
+      this.devils = this.devils.filter(function (o) {
+        return Math.abs(o.t - vm.t) > 100 && Math.abs(o.t2 - vm.t2) > 3
+      })
+    },
     dx () {
         this.t2 += 1
     },
@@ -42,6 +58,7 @@ export default {
       if (e.which == 39) {
         this.t2 += 1
       }
+      this.kill()
     }
   }
 }
@@ -50,7 +67,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #hulk {
-  position: relative;
+  position: absolute;
   width: 33vw;
+}
+
+.devil {
+  position: absolute;
+  width: 3vw;
 }
 </style>
