0761c6038bc9b4264b7fc72fb58efc51dd2ac804
Bestian Tang <bestian@gmail.com>
2021-02-12 16:28:09 +0800

    add devil

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello" @keydown="move($event)" tabindex="0" v-touch:swipe.left="dx" v-touch:swipe.right="dx2" v-touch:swipe.up="dy" v-touch:swipe.down="dy2">
-    <img id="hulk" src="/img/hulk2.jpg" :style="{top: t + 'px', left:t2 + 'vw'}">
-    <img class="devil" v-for = "(d, idx) in devils" :key="idx" src="/img/devil.jpg" :style="{top: d.t + 'px', left:d.t2 + 'vw'}">
+    <img id="hulk" src="/img/hulk2.jpg" :style="{top: t + 'px', left:t2 + 'px'}">
+    <img class="devil" v-for = "(d, idx) in devils" :id = "d.id" :key="idx" src="/img/devil.jpg" :style="{top: d.t + 'px', left:d.t2 + 'px'}">
   </div>
 </template>
 
@@ -15,48 +15,81 @@ export default {
     return {
       t: 0,
       t2: 0,
+      idx: 6,
       devils: []
     }
   },
   mounted () {
-    this.devils = [0, 1, 2, 3, 4, 5].map(function () {
+    this.devils = [0, 1, 2, 3, 4, 5].map(function (i) {
         return {
+          id: 'd' + i,
           t: Math.floor(Math.random()*500),
-          t2: Math.floor(Math.random()*100)
+          t2: Math.floor(Math.random()*window.innerWidth)
         }
     })
+    setInterval(this.randmove, 500)
+    setInterval(this.addItem, 2000)
   },
   methods: {
-    kill () {
-      var vm = this
-      this.devils = this.devils.filter(function (o) {
-        return Math.abs(o.t - vm.t) > 100 && Math.abs(o.t2 - vm.t2) > 3
+    addItem () {
+      this.devils.push({
+          id: this.idx,
+          t: Math.floor(Math.random()*500),
+          t2: Math.floor(Math.random()*window.innerWidth)
+        })
+      this.idx++
+    },
+    collide: function (el1, el2) {
+      var rect1 = el1.getBoundingClientRect();
+      var rect2 = el2.getBoundingClientRect();
+
+      return !(
+        rect1.top > rect2.bottom ||
+        rect1.right < rect2.left ||
+        rect1.bottom < rect2.top ||
+        rect1.left > rect2.right 
+      );
+    },
+    randmove() {
+      this.devils = this.devils.map(function(o) {
+        o.t += Math.floor(Math.random()*5 - 2)
+        o.t2 += Math.floor(Math.random()*5 - 2)
+        return o
       })
     },
+    kill () {
+      var collide = this.collide
+      this.devils = this.devils.filter(function (o) {
+        return !collide(document.getElementById(o.id), document.getElementById('hulk'))
+      })
+      if (this.devils.length == 0) {
+        alert('YOU WIN!!')
+      }
+    },
     dx () {
-        this.t2 += 1
+        this.t2 += 60
     },
     dx2 () {
-        this.t2 -= 1
+        this.t2 -= 60
     },
     dy () {
-        this.t += 10
+        this.t += 60
     },
     dy2 () {
-        this.t -= 10
+        this.t -= 60
     },
     move(e) {
       if(e.which == 38) {
-        this.t -= 10
+        this.t -= 60
       }
       if (e.which == 40) {
-        this.t += 10
+        this.t += 60
       }
       if(e.which == 37) {
-        this.t2 -= 1
+        this.t2 -= 60
       }
       if (e.which == 39) {
-        this.t2 += 1
+        this.t2 += 60
       }
       this.kill()
     }
@@ -68,7 +101,7 @@ export default {
 <style scoped>
 #hulk {
   position: absolute;
-  width: 33vw;
+  width: 400px;
 }
 
 .devil {
