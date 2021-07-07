0543adbd3d029a5db67b1fc342a38a023390196f
Bestian Tang 小巴 <bestian@gmail.com>
2021-07-07 19:09:31 +0800

    deploy

---
+++ precache-manifest.028db94647c52e16e7dbe3fb958f0461.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cef06c75294d8c7044c0ddbf0b87ed6",
+    "url": "/hulk/Boom.mp3"
+  },
+  {
+    "revision": "a3458d06d94e0486940cdb9a913ffc40",
+    "url": "/hulk/Boom.ogg"
+  },
+  {
+    "revision": "a7907b2745249f9d7f2b2e1a886b004a",
+    "url": "/hulk/bg.mp3"
+  },
+  {
+    "revision": "04bb5da8bad63f1d73f17b4deca0225f",
+    "url": "/hulk/bg.ogg"
+  },
+  {
+    "revision": "942f300fc1c5caba759e",
+    "url": "/hulk/css/app.9cf9a048.css"
+  },
+  {
+    "revision": "57c39cf265451f665add170c2b628de7",
+    "url": "/hulk/img/devil.57c39cf2.png"
+  },
+  {
+    "revision": "57c39cf265451f665add170c2b628de7",
+    "url": "/hulk/img/devil.png"
+  },
+  {
+    "revision": "f11ac3d85db701227423bf730ee11bef",
+    "url": "/hulk/img/hulk.f11ac3d8.png"
+  },
+  {
+    "revision": "f11ac3d85db701227423bf730ee11bef",
+    "url": "/hulk/img/hulk.png"
+  },
+  {
+    "revision": "d3dc47ffaccf63fbfef33c6b1a7cbcd7",
+    "url": "/hulk/img/hulk2.jpg"
+  },
+  {
+    "revision": "86249d024bc7c2702fb214a8b1986009",
+    "url": "/hulk/img/lose.86249d02.png"
+  },
+  {
+    "revision": "f8d3ffde187c5e6b39c65b9e482a3e34",
+    "url": "/hulk/img/win.f8d3ffde.jpg"
+  },
+  {
+    "revision": "3d1417e0123351907d302fddc7f4d286",
+    "url": "/hulk/index.html"
+  },
+  {
+    "revision": "2f9372e1ff4fcd281e60",
+    "url": "/hulk/js/about.532019e1.js"
+  },
+  {
+    "revision": "942f300fc1c5caba759e",
+    "url": "/hulk/js/app.74ceca6e.js"
+  },
+  {
+    "revision": "8eac71723f43509620bf",
+    "url": "/hulk/js/chunk-vendors.712a4bd6.js"
+  },
+  {
+    "revision": "86249d024bc7c2702fb214a8b1986009",
+    "url": "/hulk/lose.png"
+  },
+  {
+    "revision": "e7e63f6e358b8c6b346cdebe0ffa5d43",
+    "url": "/hulk/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/hulk/robots.txt"
+  },
+  {
+    "revision": "f8d3ffde187c5e6b39c65b9e482a3e34",
+    "url": "/hulk/win.jpg"
+  }
+]);
